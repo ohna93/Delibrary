b44e710cd4c1a552c4529a6aba89ee8834992369
nayou <nayou@DESKTOP-57J06G3>
2020-11-29 18:43:53 +0900

    BookDetail 오류수정....11/29 6시43분

--- Delibrary_final/src/main/java/com/example/demo/dao/BorrowDAO.java
+++ Delibrary_final/src/main/java/com/example/demo/dao/BorrowDAO.java
@@ -17,75 +17,75 @@ import com.example.demo.vo.BorrowVO2;
 
 @Repository
 public class BorrowDAO {
-	
+   
 
 
 
-	
-	
-	 public int insertBorrow(BorrowVO b) {
-		return BorrowManager.insertBorrow(b);
-	}
-	
-	
-	public int getNextNo() {
-		return BorrowManager.getNextNo();
-	}
-	//遺곸뭅�듃 紐⑸줉 異쒕젰
-	public List<BorrowVO2> selectBycust_No(HashMap map){
-		return BorrowManager.selectBycust_No(map);
-	}
-	
-	//��異쒗쁽�솴 紐⑸줉 異쒕젰[�슜�깮]
-	public List<BorrowVO2> selectBycust_No2(HashMap map){
-		return BorrowManager.selectBycust_No2(map);
-	}
-	//諛섎궔�쁽�솴 異쒕젰[�슜�깮]
-	public List<BorrowVO2> selectBycust_No3(HashMap map) {
-		return BorrowManager.selectBycust_No3(map);
-	}
+   
+   
+    public int insertBorrow(BorrowVO b) {
+      return BorrowManager.insertBorrow(b);
+   }
+   
+   
+   public int getNextNo() {
+      return BorrowManager.getNextNo();
+   }
+   //遺곸뭅 듃 紐⑸줉 異쒕젰
+   public List<BorrowVO2> selectBycust_No(HashMap map){
+      return BorrowManager.selectBycust_No(map);
+   }
+   
+   //  異쒗쁽 솴 紐⑸줉 異쒕젰[ 슜 깮]
+   public List<BorrowVO2> selectBycust_No2(HashMap map){
+      return BorrowManager.selectBycust_No2(map);
+   }
+   //諛섎궔 쁽 솴 異쒕젰[ 슜 깮]
+   public List<BorrowVO2> selectBycust_No3(HashMap map) {
+      return BorrowManager.selectBycust_No3(map);
+   }
 
-	public int update(BorrowVO b) {
-		// TODO Auto-generated method stub
-		return BorrowManager.update(b);
-	}
-	
-	public int update2(int bor_no) {
-		return BorrowManager.update2(bor_no);
-	}
-
-
-	public int delete(int bor_no) {
-		// TODO Auto-generated method stub
-		return BorrowManager.delete(bor_no);
-	}
-	public int calB_no(int cust_no) {
-		return BorrowManager.calB_no(cust_no);
-	}
-	// 紐⑤뱺 �젙蹂� 媛��졇�삤湲�[�옱�꽦]
-	public List<BorrowVO> findAll() {
-		return BorrowManager.findAll();
-
-	}
+   public int update(BorrowVO b) {
+      // TODO Auto-generated method stub
+      return BorrowManager.update(b);
+   }
+   
+   public int update2(int bor_no) {
+      return BorrowManager.update2(bor_no);
+   }
 
 
-	public int getTotalCount2(HashMap map) {
-		// TODO Auto-generated method stub
-		return BorrowManager.getTotalCount2(map);
-	}
-	
-	public int getTotalCount3(HashMap map) {
-		// TODO Auto-generated method stub
-		return BorrowManager.getTotalCount3(map);
-	}
-	
-	public int delay(int cust_no) {
-		// TODO Auto-generated method stub
-		return BorrowManager.delay(cust_no);
-	}
-	public int delay2(int cust_no) {
-		// TODO Auto-generated method stub
-		return BorrowManager.delay2(cust_no);
-	}
+   public int delete(int bor_no) {
+      // TODO Auto-generated method stub
+      return BorrowManager.delete(bor_no);
+   }
+   public int calB_no(int cust_no) {
+      return BorrowManager.calB_no(cust_no);
+   }
+   // 紐⑤뱺  젙蹂  媛  졇 삤湲 [ 옱 꽦]
+   public List<BorrowVO> findAll() {
+      return BorrowManager.findAll();
+
+   }
+
+
+   public int getTotalCount2(HashMap map) {
+      // TODO Auto-generated method stub
+      return BorrowManager.getTotalCount2(map);
+   }
+   
+   public int getTotalCount3(HashMap map) {
+      // TODO Auto-generated method stub
+      return BorrowManager.getTotalCount3(map);
+   }
+   
+   public int delay(int cust_no) {
+      // TODO Auto-generated method stub
+      return BorrowManager.delay(cust_no);
+   }
+   public int delay2(int cust_no) {
+      // TODO Auto-generated method stub
+      return BorrowManager.delay2(cust_no);
+   }
 
 }
